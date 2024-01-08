896604e96bfc2b5cef76068aaca9faca986df631
BradleyGilden <bradleygilden@gmail.com>
2024-01-08 18:56:30 +0200

    used Int8Array instead

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  // throw error if value does not occupy 8 bits
-  if (value > 127 || value < -128) throw new Error('Position outside range');
-
+  if (position >= length) throw Error('Position outside range');
   const buffer = new ArrayBuffer(length);
-  const v1 = new DataView(buffer);
-  v1.setInt8(position, value);
-  return v1;
+  const newView = new DataView(buffer, 0, length);
+  const arr = new Int8Array(buffer);
+  arr[position] = value;
+  return newView;
 }
