790f6b5916536b946010f14d3a92fbf2c4f15e61
BradleyGilden <bradleygilden@gmail.com>
2024-01-10 17:33:26 +0200

    class Interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,3 +35,29 @@ console.log(director1);
 function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`
 }
+
+// Interface with StudentClass
+interface StudentInterface {
+  firstName: string,
+  lastName: string,
+  workOnHomework(): string,
+  displayName(): string
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.lastName;
+  }
+}
