808e262124a675da91af70e14df6f9ce7a67d9f6
BradleyGilden <bradleygilden@gmail.com>
2024-01-10 15:47:13 +0200

    extensions and functions

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,7 +1,9 @@
 interface Teacher {
+  // readonly property
   readonly firstName: string,
   readonly lastName: string,
   fullTimeEmployee: boolean,
+  // optional property
   yearsOfExperience?: number,
   location: string,
   [key: string]: any
@@ -16,3 +18,20 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// extends Teacher interface
+interface Directors extends Teacher { numberOfReports: number };
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+/// returns first letter of firstname + lastname e.g. J. Doe
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`
+}
