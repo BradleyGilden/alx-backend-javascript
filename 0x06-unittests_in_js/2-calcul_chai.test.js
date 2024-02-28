629767530a7da7a656f9455c443f2d12ac2febc3
BradleyGilden <bradleygilden@gmail.com>
2024-02-28 22:14:43 +0200

    test different file

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 const {it, describe} = require('mocha');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul');
 
 describe('calculateNumber', () => {
   it('should return the correct sum of a and b if they are integers', () => {
