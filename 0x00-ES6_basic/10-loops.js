944c806e48679004ea649fe78d8afb95fbea5b61
BradleyGilden <bradleygilden@gmail.com>
2025-04-19 18:33:17 +0200

    improve looping array

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
+  const arr = [];
   for (const value of array) {
-    newArray.push(appendString + array[array.indexOf(value)]);
+      arr.push(appendString.concat(value))
   }
-
-  return newArray;
+  return arr
 }
