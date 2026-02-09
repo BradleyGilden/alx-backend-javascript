157f1c40ccb173c167f9e224882be687a183db7e
BradleyGilden <bradleygilden@gmail.com>
2026-02-09 21:01:46 +0200

    graph api

--- api-handling/test.js
+++ api-handling/test.js
@@ -23,7 +23,33 @@ const xConfig = {
     }
 }
 
-const res = await rqst('post', '/requests', xConfig)
+const xConfigGraph = {
+    baseURL: 'https://graphql.xurrent.qa',
+    headers: {
+        'content-type': 'application/json',
+        'x-xurrent-account': 'helphubtech',
+        'Authorization': TOKEN
+    },
+    data: {
+        query: `mutation {
+    requestCreate(input: { subject: "Laptop Request", serviceInstanceId: "NG1lLnFhL1NlcnZpY2VJbnN0YW5jZS8yMzQ0NjA", note: "from graphql", category: "rfi" }) {
+        errors {
+            path
+            message
+        }
+        request {
+            id
+            requestId
+            subject
+        }
+    }
+}`
+    }
+}
+
+const res = await rqst('post', '', xConfigGraph)
 console.log(res.status)
 console.log(res.headers)
 console.log(res.data);
+console.log(JSON.stringify(res.data.data.requestCreate.request, null, 2));
+//console.log(JSON.stringify(res.data.data.requestCreate.errors, null, 2));
